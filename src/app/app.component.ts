e17c90abb0d207fc7cf15f686259e69544860c26
Dan <dan.geffroy@capgemini.com>
2017-03-17 14:25:29 +0100

    having fun with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Message} from './message';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'It will be magnificent soon!';
+  private allMessages : Message[] = [{author:"yolo", text:"swagg"}];
+  private currentMessage : string = "";
+  private userName : string = "Change Me";
+  private onlineUsers : string[];
+
+  newMessage(){
+    let messageToSend : Message = {author:this.userName, text:this.currentMessage};
+    this.allMessages.push(messageToSend);
+
+    /*Clear*/
+    this.currentMessage = "";
+  }
 }

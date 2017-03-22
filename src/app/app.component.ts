23b0567e23b1decdfbbed1bbadeddbeede4ae7fc
Dan <dan.geffroy@capgemini.com>
2017-03-22 11:31:56 +0100

    implementing message history

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,11 @@ import { MessagesService } from './messages.service';
 })
 
 export class AppComponent implements OnInit, OnDestroy{
-  private allMessages : {}[] = [{author: 'yolo', text: 'swagg'}];
+  private allMessages : {}[];
   private currentMessage : string = "";
   private userName : string = "Anonymous";
   private onlineUsers : string[];
+  private errorMessage: string;
 
   connection;
 
@@ -28,6 +29,7 @@ export class AppComponent implements OnInit, OnDestroy{
   }
 
   ngOnInit() {
+    this.getMessagesHistory();
     this.connection = this.messagesService.getMessages().subscribe(message => {
       this.allMessages.push(message);
     })
@@ -35,5 +37,12 @@ export class AppComponent implements OnInit, OnDestroy{
 
   ngOnDestroy() {
     this.connection.unsubscribe();
+  }
+
+  getMessagesHistory() {
+  this.messagesService.getMessagesHistory()
+                      .then(
+                        messagesHistory => this.allMessages = messagesHistory,
+                        error =>  this.errorMessage = <any>error);
   }
 }
--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -3,17 +3,26 @@ import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import * as io from 'socket.io-client';
 import {Message} from './message';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
 @Injectable()
 export class MessagesService {
   private url = 'http://localhost:3000';
   private socket;
 
 
-  constructor() { }
+  constructor(private http: Http) { }
   sendMessage(message: Message){
     this.socket.emit('new message', message);
   }
 
+  getMessagesHistory(): Promise<Message[]>{
+  return this.http.get(this.url+'/messages')
+            .toPromise()
+            .then(response => response.json() as Message[])
+            .catch(this.handleError);
+}
   getMessages() {
     let observable = new Observable(observer => {
       this.socket = io(this.url);
@@ -26,4 +35,10 @@ export class MessagesService {
     })
     return observable;
   }
+
+
+  private handleError(error: any): Promise<any> {
+  console.error('An error occurred', error); // for demo purposes only
+  return Promise.reject(error.message || error);
+}
 }

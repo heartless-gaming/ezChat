997ea1b47372a2619fe7c7951da0c672e41af403
Florian Ledru <skullm4sher@gmail.com>
2017-03-22 16:15:52 +0100

    :skull: added a limit to the number of message in the application

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 // Require some deps
-
 let path = require('path') // nodeJS path resolution module https://nodejs.org/api/path.html
-let _ = require('lodash')
+let _ = require('lodash') // helpers around javascript API
 
 // Back office / node Server Configuration variables
 let ezchatConfig = {
   serverPort: 3000,
+  messageLimit: 15,
   path: {
     root: path.join(__dirname, '..'),
     frontFolderName: 'dist',
@@ -13,7 +13,9 @@ let ezchatConfig = {
   }
 }
 
-// Setup basic express server
+/*
+ * Routing & Express Server
+ */
 let express = require('express')
 let app = express()
 let server = require('http').createServer(app)
@@ -23,24 +25,6 @@ server.listen(ezchatConfig.serverPort, function () {
   console.log('Server listening at port %d', ezchatConfig.serverPort)
 })
 
-// Test Mesage sample
-let messages = [
-  {author: 'yolo', text: 'swagg'},
-  {author: 'ADN', text: 'Skull, please stop believing in another Half-Life game.'},
-  {author: 'ADN', text: 'It\'s getting ridiculous at this point...'},
-  {author: 'Skullmasher', text: 'But I want to believe tho...'},
-  {author: 'G-Man', text: 'Prepare for unforeseen consequences'},
-  {author: 'Skullmasher', text: 'Holy Shit !'},
-  {author: 'Gordon Freeman', text: '*Gordon is mute*'},
-  {author: 'Smith', text: 'Why, Mr. Anderson? Why, why? Why do you do it? Why, why get up? Why keep fighting? Do you believe you\'re fighting... for something? For more than your survival? Can you tell me what it is? Do you even know? Is it freedom? Or truth? Perhaps peace? Could it be for love? Illusions, Mr. Anderson. Vagaries of perception. Temporary constructs of a feeble human intellect trying desperately to justify an existence that is without meaning or purpose. And all of them as artificial as the Matrix itself, although... Only a human mind could invent something as insipid as love. You must be able to see it, Mr. Anderson. You must know it by now. You can\'t win. It\'s pointless to keep fighting. Why, Mr. Anderson? Why? Why do you persist?'},
-  {author: 'Neo', text: 'Because I choose to.'},
-  {author: 'Skullmasher', text: 'https://youtu.be/_f6MRkTLT9o'},
-  {author: 'ADN', text: 'https://i.imgur.com/JavMJGH.mp4'}
-]
-
-/*
- * Routing
- */
 let frontRoute = path.join(ezchatConfig.path.root, ezchatConfig.path.frontFolderName)
 app.use(express.static(frontRoute))
 
@@ -57,30 +41,64 @@ app.get('/messages', function (req, res) {
   res.send(messages)
 })
 
-// Chatroom
+/*
+ * Chatroom handling
+ */
 let userCount = 0
 let users = []
+let messages = [
+  {author: 'yolo', text: 'swagg'},
+  {author: 'ADN', text: 'Skull, please stop believing in another Half-Life game.'},
+  {author: 'ADN', text: 'It\'s getting ridiculous at this point...'},
+  {author: 'Skullmasher', text: 'But I want to believe tho...'},
+  {author: 'G-Man', text: 'Prepare for unforeseen consequences'},
+  {author: 'Skullmasher', text: 'Holy Shit !'},
+  {author: 'Gordon Freeman', text: '*Gordon is mute*'},
+  {author: 'Smith', text: 'Why, Mr. Anderson? Why, why? Why do you do it? Why, why get up? Why keep fighting? Do you believe you\'re fighting... for something? For more than your survival? Can you tell me what it is? Do you even know? Is it freedom? Or truth? Perhaps peace? Could it be for love? Illusions, Mr. Anderson. Vagaries of perception. Temporary constructs of a feeble human intellect trying desperately to justify an existence that is without meaning or purpose. And all of them as artificial as the Matrix itself, although... Only a human mind could invent something as insipid as love. You must be able to see it, Mr. Anderson. You must know it by now. You can\'t win. It\'s pointless to keep fighting. Why, Mr. Anderson? Why? Why do you persist?'},
+  {author: 'Neo', text: 'Because I choose to.'},
+  {author: 'Skullmasher', text: 'https://youtu.be/_f6MRkTLT9o'},
+  {author: 'ADN', text: 'https://i.imgur.com/JavMJGH.mp4'}
+] // Test Mesage sample
+
+// Pull the last message if the Massage Aray reach the maximum number of message configured
+let updateMessages = function (author, text) {
+  let msglength = messages.length
+  if (msglength >= ezchatConfig.messageLimit) {
+    messages.shift()
+    messages.push({author: author, text: text})
+    console.log('Massage limit as been reached. Pulling first message.')
+  } else {
+    messages.push({author: author, text: text})
+    console.log('new message from :' + author)
+    console.log('Text :' + text)
+  }
+}
+
+let addNewUser = function (author) {
+  // If the user does not exist add him to the users array
+  if (users.indexOf(author) === -1) {
+    users.push(author)
+    ++userCount
+    console.log(author + ' Added to userlist')
+  }
+}
 
 io.on('connect', function (socket) {
   let addedUser = false
 
   // when a client emits 'new message', this listens and executes
   socket.on('new message', function (data) {
-    // If the user does not exist add him to the users array
-    if (users.indexOf(data.author) === -1) {
-      users.push(data.author)
-      console.log(data.author + 'Added to userlist')
-    }
+    let author = data.author
+    let text = data.text
 
-    console.log('new message from :' + data.author)
-    console.log('Text :' + data.text)
-    ++userCount
+    addNewUser(author)
+    updateMessages(author, text)
     addedUser = true
-    messages.push({author: data.author, text: data.text})
+
     // we tell all clients to execute 'new message'
     socket.broadcast.emit('new message', {
-      author: data.author,
-      text: data.text
+      author: author,
+      text: text
     })
   })
 
@@ -117,8 +135,9 @@ io.on('connect', function (socket) {
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
     if (addedUser) {
-      --userCount
       _.pull(users, socket.author) // Does not work socket.author is not defined
+      --userCount
+      console.log(userCount + ' user connected')
       console.log(users)
 
       // echo globally that this client has left

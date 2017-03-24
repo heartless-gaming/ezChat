1c00fc99ffff665a5155ba03d390fde3055f3087
DanGeffroy <ogzmovie@gmail.com>
2017-03-24 21:38:02 +0100

    removing useless code

--- server/index.js
+++ server/index.js
@@ -170,23 +170,4 @@ io.on('connect', function (socket) {
 
     console.log('A guy just disconnected : ' + socket.id + '(' + reason + ')')
   })
-  /**
-* Get YouTube ID from various YouTube URL
-* @author: takien
-* @url: http://takien.com
-* For PHP YouTube parser, go here http://takien.com/864
-*/
-
-function YouTubeGetID(url){
-  var ID = '';
-  url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-  if(url[2] !== undefined) {
-    ID = url[2].split(/[^0-9a-z_\-]/i);
-    ID = ID[0];
-  }
-  else {
-    ID = url;
-  }
-    return ID;
-}
 })

e1e8f661b0a78bfe160523eb01b7610e50946e68
Dan <dan.geffroy@capgemini.com>
2017-04-11 14:22:46 +0200

    prevent userstop typing event to be send even if user is still typing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit, OnDestroy{
   private unreadMessageNumber : number = 0;
 
   private usersTyping : string[] = [];
-  private userIsTyping : boolean  = false;
+  private userIsTyping : number  = null;
   connection;
 
   constructor(private messagesService:MessagesService, private userService:UserService, private emojiService:EmojiService, private title: Title) {}
@@ -105,14 +105,16 @@ export class AppComponent implements OnInit, OnDestroy{
     this.unreadMessageNumber=0;
   }
   isTyping(){
-      if(!this.userIsTyping){
-        this.userService.isTyping(this.userName);
-        this.userIsTyping = true;
-        setTimeout(() => {
-          this.userService.stopTyping(this.userName);
-          this.userIsTyping = false;
-        }
-        , 2000);
+      if(this.userIsTyping){
+        clearTimeout(this.userIsTyping);
       }
+      else{
+        this.userService.isTyping(this.userName);
+      }
+      this.userIsTyping = setTimeout(() => {
+        this.userService.stopTyping(this.userName);
+        this.userIsTyping = null;
+      }
+      , 2000);
   }
 }
